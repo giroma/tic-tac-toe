635110c6e2c1c7d6c4c228e50b6c496518fcdfd0
Argir Argirov <dj.giro@gmail.com>
2017-11-14 16:07:38 -0500

    cant click same box twice

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var div = document.querySelector('div')
-  var body = document.querySelector('body')
+  var i = 1
+  document.body.addEventListener('click', function (e) {
+    if (e.target.innerText != "") return
 
+      if ((i % 2) === 1) {
+        e.target.innerHTML = 'X'
+        i++
+        divs.push(e.target.className)
+      }
+      else {
+        e.target.innerHTML = 'O'
+        i++
+        divs.push(e.target.className)
+      }
+      console.log(e.target.className);
 
-    var i = 1
-  body.addEventListener('click', function (e) {
-    if ((i % 2) === 1) {
-      e.target.innerHTML = 'X'
-      i++
-    }
-    else {
-      e.target.innerHTML = 'O'
-      i++
-    }
   })
-
 })
