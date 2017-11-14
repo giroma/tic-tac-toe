b074760dad2a4784533b849ef7f90c9ed0b234cd
Argir Argirov <dj.giro@gmail.com>
2017-11-14 15:33:15 -0500

    alternating X O enabled

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var div1 = document.querySelector('.div1')
+  var div = document.querySelector('div')
+  var body = document.querySelector('body')
 
-  for (var i = 1; i < 10; i++) {
-    var divs = ['div' + 'i']
-    div1.addEventListener('click', function () {
-      if ((i % 2) === 1) {
-        div1.innerHTML = 'X'
-        console.log('x');
-      }
-      else {
-        div1.innerHTML = 'O'
-        console.log('o');
-      }
-    })
-  }
+
+    var i = 1
+  body.addEventListener('click', function (e) {
+    if ((i % 2) === 1) {
+      e.target.innerHTML = 'X'
+      i++
+    }
+    else {
+      e.target.innerHTML = 'O'
+      i++
+    }
+  })
+
 })
